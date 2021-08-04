e589fc6c708ae10a3115f79bbae9ac9f5466306b
solutionbasewang <17781297@qq.com>
2021-08-04 11:03:38 +0800

    Create package.js

---
+++ package.js
@@ -0,0 +1,12 @@
+{
+  "name": "{{name}}",
+  "version": "{{version}}",
+  "description": "",
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "commander": "^8.1.0",
+    "download-git-repo": "^3.0.2",
+    "handlebars": "^4.7.7"
+  }
+}
